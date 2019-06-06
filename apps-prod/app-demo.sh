00a4486f68fbde6bbfa9f4ce8db4c624dd937235
ccx <x403368945@126.com>
2019-06-06 16:40:51 +0800

    测试使用linux服务启动jar包

--- apps-prod/app-demo.sh
+++ apps-prod/app-demo.sh
@@ -6,20 +6,20 @@
 APP_HOME=~/git-repository/common-mvc-parent/apps-prod
 # 应用名称(jar包名称)：{APP_NAME}.jar
 APP_NAME=app-demo
-LOG_PATH=$APP_HOME/logs/$APP_NAME.log
+LOG_PATH=$APP_HOME/logs/$APP_NAME/$APP_NAME.log
 #JVM参数
 # JVM_OPTS="-Dname=$SpringBoot  -Duser.timezone=Asia/Shanghai -Xms512M -Xmx512M -XX:PermSize=256M -XX:MaxPermSize=512M -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
 JVM_OPTS=""
 #启动参数
 # START_OPTS="--spring.profiles.active=prod"
 START_OPTS=""
-echo $APP_HOME > $LOG_PATH
-echo $APP_NAME > $LOG_PATH
-echo $LOG_PATH > $LOG_PATH
+echo $APP_HOME
+echo $APP_NAME
+echo $LOG_PATH
 
 if [ "$1" = "" ];
 then
-    echo -e "未输入操作名 {start|stop|restart|status} " > $LOG_PATH
+    echo -e "未输入操作名 {start|stop|restart|status} "
     exit 1
 fi
 
@@ -27,16 +27,16 @@ function start()
 {
     count=`ps -ef |grep java|grep $APP_NAME|grep -v grep|wc -l`
     if [ $count != 0 ];then
-        echo "$APP_NAME is running..." > $LOG_PATH
+        echo "$APP_NAME is running..."
     else
-        echo "Start $APP_NAME success..." > $LOG_PATH
+        echo "Start $APP_NAME success..."
         nohup java -jar  $JVM_OPTS $APP_NAME.jar  $START_OPTS > /dev/null 2>&1 &
     fi
 }
 
 function stop()
 {
-    echo "Stop $APP_NAME" > $LOG_PATH
+    echo "Stop $APP_NAME"
     echo `ps -ef |grep java|grep $APP_NAME|grep -v grep`
     echo `ps -ef |grep java|grep $APP_NAME|grep -v grep|wc -l`
     boot_id=`ps -ef |grep java|grep $APP_NAME|grep -v grep`
@@ -62,9 +62,9 @@ function status()
 {
     count=`ps -ef |grep java|grep $APP_NAME|grep -v grep|wc -l`
     if [ $count != 0 ];then
-        echo "$APP_NAME is running..." > $LOG_PATH
+        echo "$APP_NAME is running..."
     else
-        echo "$APP_NAME is not running..." > $LOG_PATH
+        echo "$APP_NAME is not running..."
     fi
 }
 
@@ -79,8 +79,6 @@ case $1 in
     status;;
     *)
 
-    echo -e "Usage: sh $APP_NAME.sh {start|stop|restart|status} \nExample: \nsh $APP_NAME.sh start" > $LOG_PATH
-
-tail -f LOG_PATH
+    echo -e "Usage: sh $APP_NAME.sh {start|stop|restart|status} \nExample: \nsh $APP_NAME.sh start"
 
 esac
