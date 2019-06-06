3c7d46ff0692e05d6570332538ab308deee601ed
ccx <x403368945@126.com>
2019-06-06 15:57:00 +0800

    测试使用linux服务启动jar包

--- apps-prod/app-demo.sh
+++ apps-prod/app-demo.sh
@@ -2,30 +2,31 @@
 # 启动|停止 app-demo.jar 文件；  chmod +x app-demo.sh
 # 注意：在sh文件中=赋值，左右两侧不能有空格
 
-# 应用名称
-APP_NAME=$2
-echo $APP_NAME
 #启动参数
-START_OPTS=$3
+START_OPTS=$2
 echo $START_OPTS
 
+# 应用运行目录
+APP_HOME=~/git-repository/common-mvc-parent/apps-prod
+# 应用名称
+APP_NAME=app-demo.jar
+LOG_PATH=$APP_HOME/logs/$APP_NAME.log
 #JVM参数
 # JVM_OPTS="-Dname=$SpringBoot  -Duser.timezone=Asia/Shanghai -Xms512M -Xmx512M -XX:PermSize=256M -XX:MaxPermSize=512M -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
 JVM_OPTS=""
-APP_HOME=~/git-repository/common-mvc-parent/apps-prod
-LOG_PATH=$APP_HOME/logs/$APP_NAME.log
+echo $APP_HOME
 echo $APP_NAME
 echo $LOG_PATH
 
 if [ "$1" = "" ];
 then
-    echo -e "\033[0;31m 未输入操作名 \033[0m  \033[0;34m {start|stop|restart|status} \033[0m"
+    echo -e "未输入操作名 {start|stop|restart|status} "
     exit 1
 fi
 
 if [ "$APP_NAME" = "" ];
 then
-    echo -e "\033[0;31m 未输入应用名 \033[0m"
+    echo -e "未输入应用名 "
     exit 1
 fi
 
@@ -43,14 +44,14 @@ function start()
 function stop()
 {
     echo "Stop $APP_NAME"
-    boot_id=`ps -ef |grep java|grep $APP_NAME|grep -v grep|awk '{print $2}'`
+    boot_id=`ps -ef |grep java|grep $APP_NAME|grep -v grep|awk '{print $APP_NAME}'`
     count=`ps -ef |grep java|grep $APP_NAME|grep -v grep|wc -l`
 
     if [ $count != 0 ];then
         kill $boot_id
         count=`ps -ef |grep java|grep $APP_NAME|grep -v grep|wc -l`
 
-        boot_id=`ps -ef |grep java|grep $APP_NAME|grep -v grep|awk '{print $2}'`
+        boot_id=`ps -ef |grep java|grep $APP_NAME|grep -v grep|awk '{print $APP_NAME}'`
         kill -9 $boot_id
     fi
 }
@@ -83,7 +84,7 @@ case $1 in
     status;;
     *)
 
-    echo -e "\033[0;31m Usage: \033[0m  \033[0;34m sh  $0  {start|stop|restart|status}  {SpringBootJarName} \033[0m
-\033[0;31m Example: \033[0m
-      \033[0;33m sh  $0  start esmart-test.jar \033[0m"
+    echo -e "Usage:  sh  $0  {start|stop|restart|status}  {SpringBootJarName} 
+Example: 
+      sh  $0  start esmart-test.jar "
 esac
